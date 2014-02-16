6568fd4ac10783fb3860185b512a4d937fce5809
brynhildr <jrferrera17@yahoo.com>
2014-02-16 20:25:34 +0800

    > Added <div> tag for the administrator menu in administrator_home_view.php

--- application/views/administrator_home_view.php
+++ application/views/administrator_home_view.php
@@ -2,9 +2,11 @@
 
 <?=$this->load->view('includes/header')?>
 
-	<?php if(isset($notification_message)){echo '<script> alert("You successfully created the account") </script>';} ?>
-	
-	<?=anchor(base_url('index.php/administrator/create_account'), 'Create Account')?>
-	<?=anchor(base_url('index.php/administrator/view_accounts'), 'View Accounts')?>
+	<div>
+		<?php if(isset($notification_message)){echo '<script> alert("You successfully created the account") </script>';} ?>
+		
+		<?=anchor(base_url('index.php/administrator/create_account'), 'Create Account')?>
+		<?=anchor(base_url('index.php/administrator/view_accounts'), 'View Accounts')?>
+	</div>
 
 <?=$this->load->view('includes/footer')?>
